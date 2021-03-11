59db2dbbaf6b0671f84c5329c30326aec5fa83e3
Zayac11 <brunogrbstudios@gmail.com>
2021-03-11 19:52:22 +0300

    form ui is done

--- src/Components/Container/Salary/Salary.jsx
+++ src/Components/Container/Salary/Salary.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import s from './Salary.module.scss'
+import s from './Salary.module.scss'
 import { Formik, Form, Field, ErrorMessage} from 'formik'
 import TextError from "../../../Common/TextError";
 import Graphic from "../Graphic/Graphic";
@@ -8,12 +8,15 @@ import {format} from 'date-fns'
 import {getDateOfDissertation} from "../../../Common/getDate";
 import DateInput from "../../../Common/CustomInputs/Date/DateInput";
 import NumberInput from "../../../Common/CustomInputs/Number/NumberInput";
+import arrows from './../../../assets/images/arrows.svg'
 
 const Salary = (props) => {
 
     return (
         <div className={'outer'}>
             <div className={'container'}>
+                <h1 className={s.title}>Калькулятор гарантированных выплат ППС</h1>
+                <h2 className={s.subtitle}>Заполните все необходимые поля</h2>
                 <Formik initialValues={props.initialValues}
                         validationSchema={props.validationSchema}
                         onSubmit={props.handleSubmit}
@@ -21,9 +24,10 @@ const Salary = (props) => {
                 >
                     {({ isSubmitting, setFieldValue, getFieldProps, handleChange, handleBlur, values }) =>
                     (<Form>
-                        <div>
-                            <label htmlFor="academicDegree">Статус в момент оформления</label>
+                        <div className={s.section}>
+                            <label className={s.label}  htmlFor="academicDegree">Укажите статус в момент оформления</label>
                                 <Field
+                                    className={`${s.select} ${s.selectStatus}`}
                                     component="select"
                                     id="academicDegree"
                                     name="academicDegree"
@@ -42,11 +46,13 @@ const Salary = (props) => {
                                     <option value="PreCandidate">Аспирант</option>
                                     <option value="Specialist">Специалист</option>
                                 </Field>
+                            <label className={s.arrows} htmlFor="academicDegree"><img src={arrows} alt="arrows"/></label>
                         </div>
 
-                        <div>
-                            <label htmlFor="academicDegreeCourse">Текущий курс</label>
+                        <div className={s.section}>
+                            <label className={s.label}  htmlFor="academicDegreeCourse">Укажите Ваш текущий учебный курс </label>
                                 <Field
+                                    className={`${s.select} ${s.selectCourse}`}
                                     component="select"
                                     id="academicDegreeCourse"
                                     name="academicDegreeCourse"
@@ -59,6 +65,7 @@ const Salary = (props) => {
                                         setFieldValue('dateOfDissertationDefense', format(getDateOfDissertation(today, values.academicDegree, e.target.value, 15), 'yyyy-MM-dd', ))
                                     }}
                                 >
+
                                     <option value="1">1</option>
                                     <option value="2">2</option>
                                     {
@@ -79,10 +86,12 @@ const Salary = (props) => {
                                             </>
                                     }
                                 </Field>
+                            <label className={s.arrows} htmlFor="dateOfRegistration"><img src={arrows} alt="arrows"/></label>
+
                         </div>
 
-                        <div>
-                            <label htmlFor="workExperience">Опыт работы по должности ППС (мес)</label>
+                        <div className={s.section}>
+                            <label className={s.label}  htmlFor="workExperience">Опыт работы по должности ППС (мес)</label>
                             <Field
                                 id="workExperience"
                                 name="workExperience"
@@ -93,8 +102,8 @@ const Salary = (props) => {
                             <ErrorMessage name="workExperience" component={TextError} />
                         </div>
 
-                        <div>
-                            <label htmlFor="dateOfBirth">Дата рождения</label>
+                        <div className={s.section}>
+                            <label className={s.label}  htmlFor="dateOfBirth">Укажите Вашу дату рождения</label>
                             <Field
                                 id="dateOfBirth"
                                 type="date"
@@ -105,8 +114,8 @@ const Salary = (props) => {
                         </div>
 
 
-                        <div>
-                            <label htmlFor="dateOfRegistration">Дата оформления</label>
+                        <div className={s.section}>
+                            <label className={s.label}  htmlFor="dateOfRegistration">Укажите дату оформления договора</label>
                             <Field
                                 id="dateOfRegistration"
                                 type="date"
@@ -118,19 +127,20 @@ const Salary = (props) => {
                         </div>
 
 
-                        <div>
-                            <label htmlFor="dateOfDissertationDefense">Предположительная дата защиты кандидатской диссертации</label>
-                            <Field
-                                id="dateOfDissertationDefense"
-                                type="date"
-                                name="dateOfDissertationDefense"
-                                disabled
-                                // component={Test}
-                            />
+                        <div className={s.dissertationContainer}>
+                            <label className={s.label} htmlFor="dateOfDissertationDefense">Предположительная дата защиты кандидатской диссертации: </label>
+                            {/*<Field*/}
+                            {/*    id="dateOfDissertationDefense"*/}
+                            {/*    type="date"*/}
+                            {/*    name="dateOfDissertationDefense"*/}
+                            {/*    disabled*/}
+                            {/*    // component={<span>{values.dateOfDissertationDefense}</span>}*/}
+                            {/*/>*/}
+                            <span className={s.dissertation}>{values.dateOfDissertationDefense}</span>
                             <ErrorMessage name="dateOfDissertationDefense" component={TextError} />
                         </div>
 
-                        <button type="submit" disabled={isSubmitting}>Рассчитать</button>
+                        <button className={s.submitBtn} type="submit" disabled={isSubmitting}>Рассчитать</button>
                     </Form>
                     )}
                 </Formik>
